81431b03faa4652eaa476001e68d31e2b82f55c9
vipinfh <146703492+vipinfh@users.noreply.github.com>
2023-11-20 17:10:28 +0100

    Update ChargingStation.java

--- Capstone_project/src/com/chargingstation/ChargingStation.java
+++ Capstone_project/src/com/chargingstation/ChargingStation.java
@@ -4,22 +4,31 @@ public class ChargingStation{
 	int capacity;
 	int occupied_slots;
 	private boolean wait_time_complete;
+	int Energy;
+	
 //public method to charge the car
-	public void ChargeCar(Car c)
-	{
-      if(capacity>occupied_slots)
-      {
-    	  initiate_charge(c);
-    	  occupied_slots++;
-      }
-      if(capacity==occupied_slots)
-      {
-    	  if(!wait_time_complete)
-    	  {
-    	    initiate_wait(c);
-    	  }
-      }
-	}
+	
+    public void ChargeCar(Car c) throws ChargingError {
+        
+            if (capacity > occupied_slots ) {
+                initiate_charge(c);
+                occupied_slots++;
+            }
+        try { 
+        	if (Energy <=0 ) {
+               throw new InsufficientEnergyError("Insufficient Energy to charge the car.");
+            }
+
+            if (capacity == occupied_slots) {
+                if (!wait_time_complete) {
+                    initiate_wait(c);
+                }
+            }
+        } catch (InsufficientEnergyError e) {
+            // Chain the exception
+            throw new ChargingError("Unable to charge the car.", e);
+        }
+    }
 //private method to initiate the charging
 	private void initiate_charge(Car c)
 	{
@@ -29,6 +38,8 @@ public class ChargingStation{
 			timer--;
 		}while(timer!=0);
 		occupied_slots--;
+		//Energy will get reduced by 200 on every charge
+		Energy = Energy - 200; 
 		c.chargeState = true;
 	}
 //private method to initiate the waiting	
@@ -46,7 +57,9 @@ public class ChargingStation{
 	ChargingStation()
 	{
 		this.capacity = 5;
-		this.occupied_slots =0;		
+		this.occupied_slots =0;	
+		//Fixed Energy of 1000 units
+		this.Energy = 1000;
 	}
 //getter for the occupied slot
     int getoccupiedslots()
