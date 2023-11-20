f17d8a6f3afaa1d50d9b8a4096d8a1292841e29d
Akhiln33322 <71833159+Akhiln33322@users.noreply.github.com>
2023-11-20 21:46:00 +0100

    DataLogMaintainer.java

--- Capstone_project/src/com/chargingstation/DataLogMaintainer.java
+++ Capstone_project/src/com/chargingstation/DataLogMaintainer.java
@@ -1,5 +1,5 @@
-import java.io.FileOutputStream;
-import java.io.FileOutputStream;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class DataLogMaintainer {
