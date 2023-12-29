e215c444206b7ca4e2f3f51f7e4480af0aa52f9b
Akhiln33322 <71833159+Akhiln33322@users.noreply.github.com>
2023-12-29 22:56:43 +0100

    Author: Akhil Narayanaswamy
    Unit testing for Logging class

---
+++ Capstone_project/src/com/chargingstation/LoggingTest.java
@@ -0,0 +1,64 @@
+package com.chargingstation;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class LoggingTest {
+
+    
+
+    @Test
+    public void testLogTheEvents() {
+    	int id = 1;
+        String message = "Test message";
+        String expectedFileName = "Charging_Station_1_Log_" + getCurrentDate() + ".txt";
+
+        Logging.logTheEvents(id, message);
+
+        Path filePath = Paths.get("logs", expectedFileName);
+        assertTrue(Files.exists(filePath));
+    }
+
+    @Test
+    public void testLogTheEvents1() {
+    	int id = 1;
+        String message = "Test message";
+        String expectedFileName = "Charging_Station_1_Log_" + getCurrentDate() + ".txt";
+
+        Logging.logTheEvents(id, message);
+
+        Path filePath = Paths.get("logs", expectedFileName);
+        assertFalse(Files.exists(filePath));
+    }
+    
+    @Test
+    public void testLogTheEnergyManagementEvents() {
+    	String message = "Test message";
+        String expectedFileName = "EnergyManagement_Log_" + getCurrentDate() + ".txt";
+
+        Logging.logTheEnergyManagementEvents(message);
+
+        Path filePath = Paths.get("logs", expectedFileName);
+        assertTrue(Files.exists(filePath));
+    }
+
+    @Test
+    public void testValidateDateTrue() {
+        assertTrue(Logging.ValidateDate("15-12-2023"));
+    }
+    
+    @Test
+    public void testValidateDateFalse() {
+        assertFalse(Logging.ValidateDate("invalid_date"));
+    }
+    
+    private String getCurrentDate() {
+        return new SimpleDateFormat("dd-MM-yyyy").format(new Date());
+    }
+}
