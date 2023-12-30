c0e340be93e854291cc65683194b13a8f3a4f749
krithika-premkumar <krithikapremk.4@gmail.com>
2023-12-30 22:26:12 +0100

    Added new unit tests

---
+++ Capstone_project/src/com/chargingstation/SharedResourceTest.java
@@ -0,0 +1,18 @@
+package com.timeslot.Manager;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SharedResourceTest {
+
+	@Test
+	public void testaddandremoveCar() {
+		SharedResource sharedResource = new SharedResource();
+		Car c1 = new Car(2);
+		sharedResource.addCar(c1);
+		assertEquals(sharedResource.removeCar().id,c1.id);
+
+	}
+
+}
---
+++ Capstone_project/src/com/chargingstation/UserNameTest.java
@@ -0,0 +1,43 @@
+package com.timeslot.Manager;
+import java.io.*;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserNameTest {
+
+	@Test
+	public void testUserName() {
+	    UserName u1= new UserName();
+	    ArrayList<UserName> username = new ArrayList<>();
+	    String input = "Mary\nmojo";
+	    ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+	    System.setIn(in);
+	    u1.NewUserRegistration(username);
+	    assertEquals("Mary", u1.userName);
+	}
+	
+	@Test
+	public void testPassword() {
+	    UserName u1= new UserName();
+	    ArrayList<UserName> username = new ArrayList<>();
+	    String input = "Mary\nmojo";
+	    ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+	    System.setIn(in);
+	    u1.NewUserRegistration(username);
+	    assertEquals("mojo", u1.password);
+	}
+	@Test
+	public void testUserAdded() {
+	    UserName u1= new UserName();
+	    ArrayList<UserName> username = new ArrayList<>();
+	    String input = "Mary\nmojo";
+	    ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+	    System.setIn(in);
+	    u1.NewUserRegistration(username);
+	    assertEquals(1, username.get(0).c.id);
+	}
+
+}
